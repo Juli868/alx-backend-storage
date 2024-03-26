1b9bfa568103807b9056bf8360e7ee8e937355d1
juli868 <bugijul@gmail.com>
2024-03-26 23:20:32 +0200

    Update function.

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""Data updates."""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update data according to the parameters."""
+    target = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    mongo_collection.update_one(target, new_values)
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+"""Insertion."""
+
+
 def insert_school(mongo_collection, **kwargs):
-    pass
+    """Insert into a collection and return an id of the newly added object."""
+    result = mongo_collection.insert_one(kwargs).inserted_id
+    return result
