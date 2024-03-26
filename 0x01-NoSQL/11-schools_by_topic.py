0c2188b4cf12d3f0caded3221f841c5d40f73c93
juli868 <bugijul@gmail.com>
2024-03-27 01:32:10 +0200

    download and add to a db

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """Search specifically."""
+
+
 def schools_by_topic(mongo_collection, topic):
+    """Search particularly according to the defined desire."""
     parameters = {"topics": topic}
     return mongo_collection.find(parameters)
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+"""Analyse data."""
+import requests
+import io
+import zipfile
+insert_school = __import__('9-insert_school').insert_school
+from pymongo import MongoClient
+
+
+url = 'https://s3.amazonaws.com/\
+intranet-projects-files/\
+holbertonschool-webstack/411/dump.zip'
+response = requests.get(url)
+if response.status_code == 200:
+    with zipfile.ZipFile(io.BytesIO(response.content), 'r') as zip_ref:
+        extracted_files = {
+                          name: zip_ref.read(name)
+                          for name in zip_ref.namelist()
+                          }
+client = MongoClient("mongodb://localhost:27017")
+data = client.temp
+collect = data.downloads
+insert_school(collect, **extracted_files)
+gets = data.count_documents({"get" : True})
+posts = data.count_documents({"get" : True})
+puts = data.count_documents({"put" : True})
+patchs = data.count_documents({"patch" : True})
+deletions = data.count_documents({"delete" : True})
+all_d = data.count_documents({})
+print(f'{all_d} logs\
+            Methods\
+                method GET: {gets}\
+                method POST: {posts}\
+                method PUT : {puts}\
+                method PATCH: {patchs}\
+                method DELETE: {deletions}')
