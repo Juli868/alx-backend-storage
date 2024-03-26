18b3fe7681979fa57a6def0b854795a592f4d87b
juli868 <bugijul@gmail.com>
2024-03-26 23:38:44 +0200

    Search by parameters

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ def update_topics(mongo_collection, name, topics):
     """Update data according to the parameters."""
     target = {"name": name}
     new_values = {"$set": {"topics": topics}}
-    mongo_collection.update_one(target, new_values)
+    mongo_collection.update_many(target, new_values)
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,5 @@
 #!/usr/bin/env python3
+"""Search specifically."""
+def schools_by_topic(mongo_collection, topic):
+    parameters = {"topics": topic}
+    return mongo_collection.find(parameters)
